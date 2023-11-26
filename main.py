1ec663cc686aaf0d52a8bdaebe57f026e43a6d09
Ilya Krivtsov <100132452+La1tek@users.noreply.github.com>
2023-11-26 20:59:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import os
 import subprocess
+import sys
 
-subprocess.run(['python', os.path.join(os.path.dirname(os.path.abspath(__file__)), 'src', 'Gui.py')])
+if __name__ == '__main__':
+    python_executable = 'python' if sys.version_info[0] < 3 else 'python3'
+    script_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'src', 'gui.py')
 
+    subprocess.run([python_executable, script_path])
+  
